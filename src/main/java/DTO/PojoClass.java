32ef7ecac6b0370423b169d3c1d25bf3afc28eb0
asamim123 <samim.nass@yahoo.com>
2019-12-02 18:20:15 -0500

    CFT-676 added new project

--- src/main/java/DTO/PojoClass.java
+++ src/main/java/DTO/PojoClass.java
@@ -1,8 +1,14 @@
 package DTO;
 
 public class PojoClass {
-public void show() {
-	
-	System.out.println("did some changes in the class");
-}
+	public void show() {
+
+		System.out.println("did some changes in the class");
+	}
+
+	public void display() {
+		
+		System.out.println("new change happen ");
+
+	}
 }
